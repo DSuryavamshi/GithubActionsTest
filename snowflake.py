5a6a543b6ecfa7c43dba1cf1118ae523a23da382
Dhanush Suryavamshi <suryavamshi221b@gmail.com>
2022-03-28 12:18:30 +0530

    modified config

--- snowflake.py
+++ snowflake.py
@@ -1,11 +1,15 @@
 import sys
 
 file_name = sys.argv[1]
-print(f"{file_name} has been changed")
-qyery = ""
-with open(file_name, 'r') as f:
-  query = f.readlines()
-  
-print(query)
-print("working")
-print("working")
+file_type = file_name.split('.')[1]
+if file_type.lower() = 'yml':
+  sys.exit(0)
+else:
+  print(f"{file_name} has been changed")
+  qyery = ""
+  with open(file_name, 'r') as f:
+    query = f.readlines()
+    
+  print(query)
+  print("working")
+  print("working")
--- test.sql
+++ test.sql
@@ -2,6 +2,5 @@ create or replace schema.table(
   co11 varchar,
   col2 timestamp,
   col3 date,
-  col4 varchar,
-  col5 varchar
+  col4 varchar
  )
